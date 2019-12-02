036b75d1dbe8eed2e7576cd27807326473e5566b
Aashutosh Singh <aashutoshsingh126@gmail.com>
2019-12-02 22:09:55 +0530

    Test commit

--- app/src/main/java/com/example/learnest/MainActivity.java
+++ app/src/main/java/com/example/learnest/MainActivity.java
@@ -138,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this,registrationActivity.class);
         startActivity(intent);
     }
-
+// Commit test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
